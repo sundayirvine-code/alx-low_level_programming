140b8f63eb1ad7af55eb4e924f504640d6f50560
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-08 14:06:27 -0700

    changed %zu to %d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,10 @@ int main()
     long long long_long_type;
     float float_type;
 
-    printf("size of a char: %zu bytes\n", sizeof(char_type));
-    printf("size of an int: %zu bytes\n" sizeof(int_type));
-    printf("size of a long int: %zu bytes\n" sizeof(long_type));
-    printf("size of a long long int: %zu bytes\n" sizeof(long_long_type));
-    printf("size of a float: %zu bytes\n" sizeof(float_type));
+    printf("size of a char: %d bytes\n", sizeof(char_type));
+    printf("size of an int: %d bytes\n" sizeof(int_type));
+    printf("size of a long int: %d bytes\n" sizeof(long_type));
+    printf("size of a long long int: %d bytes\n" sizeof(long_long_type));
+    printf("size of a float: %d bytes\n" sizeof(float_type));
     return(0);
 }
-
