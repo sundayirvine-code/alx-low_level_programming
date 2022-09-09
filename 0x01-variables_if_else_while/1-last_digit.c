d6ea3e0be943e09cdb6487a02a4da0a901d4c00e
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-09 11:01:50 -0700

    control flow with the last digit of an integer

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - the main function.
+ * @n - random number
+ * @l - last diigit of l and also The number to be checked
+ *
+ */
+
+int main(void)
+{
+	int n, l;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	l = n % 10;
+	if (l > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n,l);
+	}
+	if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n,l);
+	}
+	if (l < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n");
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,2 @@
-0. Positive anything is better than negative nothing - prints out to the screen whether the number n is positive, negative, or zero
-
+0. Positive anything is better than negative nothing - a C program that prits out whether the value n is positive negative or zero
+1. The last digit - Print the last digit of a random number
