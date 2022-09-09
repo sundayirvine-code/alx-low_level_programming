b1d20e2db187984d9aa4f7441d54c81f653b74e8
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-09 11:40:45 -0700

    int i declaration

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,9 +6,10 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
+	int i;
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 	/* your code goes there */
-	while (int i = 0; i< 27; i++)
+	while (i = 0; i< 27; i++)
 		putchar(alphabet[i]);
 	putchar('\n');
 	return (0);
