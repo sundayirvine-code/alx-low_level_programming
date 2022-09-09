70998b8b5f7cd76bea55f4c0efe08727e20263df
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-09 11:12:11 -0700

    Adhere to betty style coding

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,6 +8,7 @@
  * @n - random number
  * @l - last diigit of l and also The number to be checked
  *
+ * return 0 for the main function
  */
 
 int main(void)
@@ -20,15 +21,15 @@ int main(void)
 	l = n % 10;
 	if (l > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n,l);
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
 	if (l == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n,l);
+		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
 	if (l < 6 && l != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,l);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
 	return (0);
 }
