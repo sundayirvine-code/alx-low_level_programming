10cb4165cb157bc36b85d735664e0e2cca810d7c
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-09 11:04:59 -0700

    corrected format of code so that %d includes a matching integer

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ int main(void)
 	}
 	if (l < 6)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,l);
 	}
 	return (0);
 }
