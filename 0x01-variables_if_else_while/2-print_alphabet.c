278e38ed49ef53b550adfc6d0c17757b9584bca5
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-09 11:36:51 -0700

    corrected string assigneent

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,10 +6,10 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	char alphabet[abcdefghijklmnopqrstuvwxyz];
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 	/* your code goes there */
-	for (int i = 0; i< 26; i++)
-		putchar(str[i]);
+	for (int i = 0; i< 27; i++)
+		putchar(alphabet[i]);
 	putchar('\n');
 	return (0);
 }
