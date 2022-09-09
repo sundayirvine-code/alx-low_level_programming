55a4a523160f9f7cf0faf81110bb61e5c619e0c2
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-09 11:07:33 -0700

    added and to check if l is not equla to zero

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n,l);
 	}
-	if (l < 6)
+	if (l < 6 && l != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,l);
 	}
