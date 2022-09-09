a4aecf3567ad3b1da851dfea5203a41231b12d0a
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-09 11:23:34 -0700

    print all the alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char alphabet[abcdefghijklmnopqrstuvwxyz];
+	/* your code goes there */
+	for (int i = 0; i< 26; i++)
+		putchar(str[i]);
+	putchar('\n');
+	return (0);
+}
