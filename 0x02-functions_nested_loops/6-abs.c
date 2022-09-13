3e394d36a4e96ee6b27ae6997059ab95df4b5f04
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-13 13:44:36 -0700

    remove main

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -17,17 +17,3 @@ int _abs(int b)
 	return abs(b);
 }
 
-int main(void)
-{
-    int r;
-
-    r = _abs(-1);
-    printf("%d\n", r);
-    r = _abs(0);
-    printf("%d\n", r);
-    r = _abs(1);
-    printf("%d\n", r);
-    r = _abs(-98);
-    printf("%d\n", r);
-    return (0);
-}
