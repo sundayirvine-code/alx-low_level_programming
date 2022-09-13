be0318d155f66bddf2ae4903987bf4432f6b8f78
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-13 13:33:36 -0700

    betty styling

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,10 +15,10 @@
 void print_alphabet_x10(void)
 {
         int i;
-	for(i=0; i<10; i++)
+	for(i = 0; i < 10; i++)
 	{
 		char letter;
-        	for(letter='a'; letter<='z'; letter++)
+        	for(letter = 'a'; letter <= 'z'; letter++)
         	{
                 	_putchar(letter);
                 
