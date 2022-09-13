b049a2d405d99b80dd440f1e1edc96c5e6df743d
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-12 23:15:21 -0700

    rename the files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,13 @@
-#include <stdio.h>
-#include "main.h"
-/*
- *main - Entry point
- *Return: Always 0 (success)
- */
+#include <unistd.h>
 
-int main(void)
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	putchar("\n");
-	return (0);
+	return (write(1, &c, 1));
 }
