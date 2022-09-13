0b86debb6929d232b0671342e96f637036583b2f
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-13 14:10:18 -0700

    corrected loop

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -28,7 +28,7 @@ void print_to_98(int n)
         }
         else if (n > 98)
         {       
-                for(n>=98)
+                while(n>=98)
                 {       
                         if(n == 98)
                         {
