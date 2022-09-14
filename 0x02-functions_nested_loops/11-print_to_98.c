db55055032df2f9515af010e5b8c9dd1b299b338
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-14 03:28:47 -0700

    betty

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 /**
  * main:check the code
- * print_to_98: prints to 98
+ * print_to_98:prints to 98 from value n
  *
  * @n: value to check against
  *
@@ -14,9 +14,9 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while(n <= 98)
+		while (n <= 98)
 		{
-			if(n==98)
+			if(n == 98)
 				printf("%d", n);
 			else
 				printf("%d, ", n);
