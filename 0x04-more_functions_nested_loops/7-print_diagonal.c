11b57133c1b84d9144da3e8b4fafafc9271fee79
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-15 12:51:52 -0700

    loop fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -23,9 +23,9 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (i = 1; i < n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for (j = 1; j < n; j++)
+			for (j = 1; j < i; j++)
 			{
 				_putchar(' ');
 			}
