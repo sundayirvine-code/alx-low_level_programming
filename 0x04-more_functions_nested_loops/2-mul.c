c674999c05132e2694957b22382ed9c625350dab
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-15 04:51:08 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * int mul(int a, int b): function that multiplies 
+ * two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: The product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,5 +15,8 @@ int add(int one, int two);
 void print_to_98(int n);
 void print_times_table(int n);
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+
 
 #endif /* HOLBERTON_H */
