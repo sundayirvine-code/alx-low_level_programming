7317118829d82dc32807b36253d0d61ce4d3af5e
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-15 11:56:51 -0700

    fizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - prints fizz buzz
+ *
+ * @i - loops 100 times
+ * @f - holds the string fizz
+ * @b - holds the string Buzz
+ * @fb - holds the string fizzbuzz
+ *
+ * Return - Always 0
+ */
+
+int main()
+{
+    int i;
+    char *f = "Fizz";
+    char *b = "Buzz";
+    char *fb = "FizzBuzz";
+    
+    for (i = 1; i < 101; i++)
+    {
+        /* if multiple of 3*/
+        if ((i % 3) == 0 && (i % 5) == 0)
+        {
+            printf("%s ", fb);
+            continue;
+        }
+        if ((i % 3) == 0)
+        {
+            printf("%s ", f);
+            continue;
+        }
+        if ((i % 5) == 0)
+        {
+            printf("%s ", b);
+            continue;
+        }
+        else
+        {
+            printf("%d ", i);
+        }
+        
+        
+    }
+
+i    return 0;
+}
