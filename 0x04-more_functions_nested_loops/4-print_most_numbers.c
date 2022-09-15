8fedee908c4d6b6b67234d4d19ae4dde470c19ef
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-15 12:33:15 -0700

    dont print 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -15,11 +15,15 @@ void print_most_numbers(void)
 
         for (i = 0; i < 10; i++)
         {
-		if (i != 2 || i != 4)
+		if (i == 2 || i == 4)
 		{
-                	_putchar(i + '0');
+                	
 		}
 
+		else
+		{
+			_putchar(i + '0');
+		}
         }
         _putchar('\n');
 
