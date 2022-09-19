00ee2f2b9d5138f2464956d7be907422df199aa9
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-19 03:30:06 -0700

    find length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function that returns the length of a string
+ * 
+ * @*s - stores the pointer to a string
+ * @l - length of the string
+ *
+ * Retrun: Always n
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	l = strlen(*s);
+	
+	return (l);
+}
