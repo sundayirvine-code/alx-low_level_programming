5672afdb98f960ed6cb492bef56350188668951b
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-19 12:44:21 -0700

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copes a string
+ * @src: pointer
+ * @dest: another pointer
+ * Return: the pointer to dest
+ */
+char *__strcpy(char *dest, char *src)
+{
+	strcpy(dest , scr);
+	return (dest);
+}
+		
