d4a351003c25c18be3a548f50bba13640ff8cff9
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-19 02:28:45 -0700

    pointers task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to98 -function that takes a pointer
+ * and updates its value to 98
+ *
+ * @*n - pointer variable
+ *
+ * Return: n. always 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	return n;
+}
