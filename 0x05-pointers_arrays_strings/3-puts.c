8f6ae0c1592c6a91d57ab3e25c615467c9f9f1c9
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-19 06:18:17 -0700

    correct string declaration

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,18 @@
 #include "main.h"
 #include <string.h>
+
 /**
  * _puts - function that prints a string
- *
  * @*str - pointer to the string
- *
  * Return: Always void
  */
 
 void _puts(char *str)
 {
-	while (*s != '\0')
+	while (*str != '\0')
 	{
-		_putchar(*s + '0');
-		s++;
+		_putchar(*str);
+		str++;
 	}
 	_putchar('\n');
 }
