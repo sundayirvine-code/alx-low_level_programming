bf78515e266e72b80465f3072f112c6f73ca8317
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-26 10:52:59 -0700

    a function that searches a string for any set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk - function that searches a string for any set of bytes
+ * @s: This is the C string to be scanned.
+ * @accept: This is the C string containing the characters to match
+ *
+ * Return: s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	strpbrk(s, accept);
+	return (s);
+}
+
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@
 1. memcpy - a function that copies memory area.
 2. strchr -  a function that locates a character in a string.
 3. strspn - a function that gets the length of a prefix substring.
+4. strpbrk - a function that searches a string for any of a set of bytes.
