6e086424fc6eeab5d909c123d89643ea7bfe0920
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-26 11:33:55 -0700

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,15 +5,15 @@
  * @a: matrix
 */
 
-void print_chessboard(char (*a)[8])
+void print_chessboard(char b(*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < a; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j]);
+			_putchar(b[i][j]);
 		}
 	_putchar('\n');
 	}
