4f2599567c211c8a212afc8632ed0c672564c2a9
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-27 11:56:30 -0700

    print string to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function that prits given text
+ *
+ * Retur: Alway 0
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
