5ee6c57aea1be0f1bcbf0f122c5d863fd45e2ded
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-28 09:41:48 -0700

    Betty coding style corection

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,13 +5,13 @@
  * _strlen_recursion - retuns the length of s
  * @s: input string
  *
- * Rteurn: length of s
+ * Return: length of s
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 
 }
