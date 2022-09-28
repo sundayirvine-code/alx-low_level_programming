9195ad448173622a022360e1fb602a1daabdc583
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-28 11:58:12 -0700

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,32 +10,41 @@
 
 int prime_number(int n, int i)
 {
-	if (n == 2)
-		return (1);
+    if (n == 2)
+    {
+        return (1);
+    }
+    if (n == 1)
+    {
+        return (0);
+    }
+    
+    if (n <= 0)
+    {
+        return (0);
+    }
 
-	if (n == 1)
-		return (0);
-
-	if (n <= 0)
-		return (0);
-
-	/* terminating condition */
-	if (i > n)
-		return (1);
-
-	if (i != 1)
-	{
-		if (i != n)
-		{
-			if (n % i == 0)
-			{
-				/* not prime */
-				return (0);
-			}
-		}
-	}
-	i = i + 1;
-	prime_number(n, i);
+    if (i > n)
+    {
+        return (1);
+    }
+    
+    if (i != 1)
+    {
+        if (i != n)
+        {
+            if (n % i == 0)
+            {
+                return (0);
+            }
+            
+            
+        }
+        
+    }
+    i = i + 1;
+    prime_number(n, i);
+    
 }
 
 /**
