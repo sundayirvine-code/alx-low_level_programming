2591f8a11bc1a58a417c9b0bfd2da7012a91090f
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-28 12:11:16 -0700

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,50 +10,38 @@
 
 int prime_number(int n, int i)
 {
-    if (n == 2)
-    {
-        return (1);
-    }
-    if (n == 1)
-    {
-        return (0);
-    }
-    
-    if (n <= 0)
-    {
-        return (0);
-    }
+	if (n == 2)
+		return (1);
 
-    if (i > n)
-    {
-        return (1);
-    }
-    
-    if (i != 1)
-    {
-        if (i != n)
-        {
-            if (n % i == 0)
-            {
-                return (0);
-            }
-            
-            
-        }
-        
-    }
-    i = i + 1;
-    prime_number(n, i);
+	if (n == 1)
+		return (0);
 
-    return (1);
-    
+	if (n <= 0)
+		return (0);
+
+	if (i > n)
+		return (1);
+
+	if (i != 1)
+	{
+		if (i != n)
+		{
+			if (n % i == 0)
+			{
+				return (0);
+			}
+		}
+	}
+	
+	i = i + 1;
+	prime_number( n, i);
+
+	return (1);
 }
 
 /**
  * is_prime_number - checks if a number is prime or not
  * @n: test integer
- * @i: counter
- * @prime: return value
  *
  * Return: 1 if prime 0 of not
  */
