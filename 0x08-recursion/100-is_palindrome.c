59b003bc19c04b338ad297a92f8c4f6b180e939b
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-29 10:52:14 -0700

    check if string is palindrome or not

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * palindrome - returns 1 if s is a palindrome otherwise 0
+ * @s: string to check
+ * @loop_counter: number of iterations to perform
+ * @first: tracks index fro left to right
+ * @last: tracks index from roght to left
+ *
+ * Return: 1 if palindrome 0 otherwise
+ */
+
+int palindrome(char *s, int loop_counter, int first, int last)
+{
+	if (first > loop_counter)
+		return (1);
+
+	if (s[first] != s[last])
+		return (0);
+
+	first = first + 1;
+	last = last - 1;
+
+	palindrome(s, loop_counter, first, last);
+}
+
+/**
+ * is_palindrome - checks if s is a palindrome
+ * @s: input strig to check
+ *
+ * Return: 1 if s is palindrome 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	int last, first, loop_counter, len, ret;
+	len = strlen(s);
+	loop_counter = len / 2;
+	first = 0;
+	last = len - 1;
+
+	ret = palindrome(s, loop_counter, first, last);
+	return (ret);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -5,3 +5,4 @@
 4 - a function that returns the value of x raised to the power of y
 5 - a function that returns the natural square root of a number.
 6 - a function that returns 1 if the input integer is a prime number, otherwise return 0
+7 - a function that returns 1 if a string is a palindrome and 0 if not.
