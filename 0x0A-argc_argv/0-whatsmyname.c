c217df17409f0b1840cc3813c0c4d023c242dfc6
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-30 09:29:35 -0700

    print name of function

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: arguemnt vector
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+#endif /* HOLBERTON_H */
