43ed5cf9d5e3e0231cb5068a61845ca6ed2b4da2
sundayirvine-code <amugumbiirvine@gmail.com>
2022-09-30 09:38:34 -0700

    print function name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,9 +10,9 @@
  * Return: always 0
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
-	return (0);
+	return (0)i;
 }
