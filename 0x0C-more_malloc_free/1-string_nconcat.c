98e86b9f93a195a780bc9014706d8e86df80154a
sundayirvine-code <amugumbiirvine@gmail.com>
2022-10-06 13:05:13 -0700

    a function that creates an array of integers

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,20 +3,6 @@
 #include <string.h>
 
 /**
- * _strlen - calculate and return string length
- * @string: string
- * Return: string length
- */
-int _strlen(char *string)
-{
-	int i;
-
-	for (i = 0; string[i] != '\0'; i++)
-		;
-	return (i);
-}
-
-/**
  * string_nconcat - function that concatenates two strings'
  * @s1: string 1
  * @s2: string 2
@@ -30,36 +16,34 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	unsigned int l1, l2, sum, i, j;
 
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
+	l1 = strlen(s1);
+	l2 = strlen(s2);
 
 	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	if (n < 0)
-		return (NULL);
-
-	if (n >= 12)
-		n = l2;
+		l1 = 0;
+	if(s2 == NULL)
+		l2 = 0;
 
 	sum = l1 + n + 1;
 
+	if (n >= l2)
+		sum = l1 + l2 + 1;
+
 	s = malloc(sizeof(char) * sum);
 
-	if ( s == NULL)
+	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1 != '\0'; i++)
+	{
 		s[i] = s1[i];
+	}
 
 	for (j = 0; j < n; j++)
+	{
 		s[i + j] = s2[j];
-
+	}
 	s[i + j] = '\0';
 
 	return (s);
 }
-
