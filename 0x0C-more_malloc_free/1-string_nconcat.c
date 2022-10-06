815d4433c4994d20d688cf4f7e8212037f837b64
sundayirvine-code <amugumbiirvine@gmail.com>
2022-10-06 13:00:10 -0700

    a function that creates an array of integers

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,6 +3,20 @@
 #include <string.h>
 
 /**
+ * _strlen - calculate and return string length
+ * @string: string
+ * Return: string length
+ */
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
  * string_nconcat - function that concatenates two strings'
  * @s1: string 1
  * @s2: string 2
@@ -16,8 +30,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	unsigned int l1, l2, sum, i, j;
 
-	l1 = strlen(s1);
-	l2 = strlen(s2);
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
 
 	if (s1 == NULL)
 		s1 = "";
