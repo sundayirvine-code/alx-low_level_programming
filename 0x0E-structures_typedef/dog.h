0aeac9d9c35f0e37054dc4c750666e31be774c87
sundayirvine-code <amugumbiirvine@gmail.com>
2022-10-11 11:48:55 -0700

    correction on dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
  * struct dog - a data structure of a dog
@@ -14,3 +14,12 @@ struct dog
 	char *owner;
 	float age;
 };
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(dog_t *d);
+void free_dog(dog_t *d);
+
+#endif
+
