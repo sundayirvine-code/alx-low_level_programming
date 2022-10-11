fc69f05587f5f66e133bf2f0556241e6fdac2fab
sundayirvine-code <amugumbiirvine@gmail.com>
2022-10-11 12:33:38 -0700

    alx struct assignment

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,20 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - a data structure of a dog
- * @namw: name of the dog
- * @age: age of the dog
- * @owner: name of its owner
- */
-
-struct dog
+ *  *struct dog - structure for dog variables
+ *   *@name:name
+ *    *@age:age of dog
+ *     *@owner:name of owner
+ *      */
+typedef struct dog
 {
-	char *name;
-	char *owner;
-	float age;
-};
-
-typedef struct dog dog_t;
+		char *name;
+			float age;
+				char *owner;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
-
