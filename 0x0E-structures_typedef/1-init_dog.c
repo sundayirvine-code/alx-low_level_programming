678d15378162f58464026f6d320d38aeba468fa8
sundayirvine-code <amugumbiirvine@gmail.com>
2022-10-11 13:18:27 -0700

    prit struct elements

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,19 +1,20 @@
-#include <stdlib.h>
-#include <stdio.h>
 
+#include "dog.h"
 /**
- * int_dog - function that initializes a variable of type struct dog
- * @d: variable to be initialized
- * @name: dog's name
- * @age: dogs's age
- * @owner: dogs's owner
- *
- * Return: Nothing
- */
-
+ *  *init_dog -initializes a variable of type dog
+ *   *
+ *    *@d:struct
+ *     *@name:name of dog
+ *      *@age:age of dog
+ *       *@owner:name of owner
+ *        *Return:void
+ *         */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d -> name = name;
-	d -> age = age;
-	d -> owner = owne;;
+		if (d != '\0')
+				{
+							(*d).name = name;
+									(*d).age = age;
+											(*d).owner = owner;
+												}
 }
