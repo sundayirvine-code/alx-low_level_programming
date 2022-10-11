ce9138a9c67095f7ffc0e2eeb4c5063144f58352
sundayirvine-code <amugumbiirvine@gmail.com>
2022-10-11 11:16:45 -0700

    adhered to betty documentation style for a struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct dog - a data structure of a dog
+ * @namw: name of the dog
+ * @age: age of the dog
+ * @owner: name of its owner
+ */
+
 struct dog
 {
 	char *name;
