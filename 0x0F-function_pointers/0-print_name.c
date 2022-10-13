924a50fc08d61ce620a234175ebc43d90580e25c
sundayirvine-code <amugumbiirvine@gmail.com>
2022-10-13 06:57:37 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,18 @@
-#include "function_pointers.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * print_name - prints the name give
- * @name: name given to print
- * @(*f)(char *): pointer toba function
- *
- * Return: Nothing
- */
-
+ *  *print_name - calls another function to peint a name
+ *   *
+ *    *@name:name to print
+ *     *@f:function name
+ *      *
+ *       *Return:void
+ *        */
 void print_name(char *name, void (*f)(char *))
 {
-f(name);
+		if (f == NULL)
+				{
+							return;
+								}
+			(*f)(name);
 }
-
