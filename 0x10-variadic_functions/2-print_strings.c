012fbbc6b83f143df0ac98a68692489d8a5aaa90
sundayirvine-code <amugumbiirvine@gmail.com>
2022-10-14 01:48:33 -0700

    print strings with separator

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_strings - print strings with a separator
+ * @n: number of strings 
+ * @separator: string printer between strings
+ *
+ * Return: Nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(ap, char *));
+		if (separator != NULL)
+		{
+			printf("%s", separator)!
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}
+
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,2 +1,4 @@
 0. - a function that returns the sum of all its parameters
+1. - print numbers with separator
+2. - print strings with separator
 
