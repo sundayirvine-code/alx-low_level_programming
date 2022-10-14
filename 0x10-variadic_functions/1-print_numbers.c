8898fb3ac775e20f01cd915efb31919bbc61b134
sundayirvine-code <amugumbiirvine@gmail.com>
2022-10-14 04:56:25 -0700

    include stdarg lib

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(ap, int));
 
-		if (separator != NULL)
+		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
 		}
