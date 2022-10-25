b6a0641a260045201a10381d7c365fd25df302aa
sundayirvine-code <amugumbiirvine@gmail.com>
2022-10-24 18:29:38 -0700

    linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdlib.h>
 #include <string.h>
 
-size_t print_list(const list_t *h);
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
