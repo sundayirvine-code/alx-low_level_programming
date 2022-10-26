30420004210955342db37625dfd3d25d42670edb
sundayirvine-code <amugumbiirvine@gmail.com>
2022-10-26 15:02:34 -0700

    initial commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a list.
+ * @h: pointer to the start of the list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+	listint_t *ptr;
+
+	count = 0;
+	ptr = h;
+
+	while (ptr)
+	{
+		printf("%d\n", ptr -> n);
+		ptr = ptr -> next;
+		count++;
+	}
+	return (count);
+}
+
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+more on singly linked lists
+
