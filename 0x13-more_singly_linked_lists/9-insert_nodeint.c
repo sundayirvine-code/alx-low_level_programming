4525161222eb29e9446d70d98c54f4473b6ddd1c
sundayirvine-code <amugumbiirvine@gmail.com>
2022-10-27 12:25:00 -0700

    a function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,59 +1,40 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position.
- * @head: double pointer to list
- * @idx: position of insertion
- * @n: data of the new node
- *
- * Return: address of the new node, or NULL if it failed
- */
+ * insert_nodeint_at_index - Insert a new node at a given positiion.
+ * @head: First node address.
+ * @idx: Position of the new node to be inserted in.
+ * @n: Data of the new node.
+ * Return: Address of the new node.
+ **/
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	int node_count;
-	listint_t *temp, *new_node;
+	listint_t *new_node, *temp;
+	unsigned int i = 0;
 
-	/* check if list exists */
-	if(!*head)
+	if (*head == NULL && idx != 0)
 		return (NULL);
-
-	/* count number of nodes */
-	temp = *head;
-	node_count = 0;
-
-	while (temp)
+	if (idx != 0)
 	{
-		node_count++;
-		temp = temp -> next;
-	}
-
-	/* check if it is possible to add new node*/
-	if (idx >= node_count)
+	temp = *head;
+	for (; i < idx - 1 && temp != NULL; i++)
+		temp = temp->next;
+	if (temp == NULL)
 		return (NULL);
-
-	/* create new node */
+	}
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-
-	new_node -> n = n;
-	new_node -> next = NULL;
-
-	node_count = 0;
-	temp = *head;
-
-	/* traverse list */
-	while (node_count != idx - 1)
+	new_node->n = n;
+	if (idx == 0)
 	{
-		temp = temp -> next;
-		node_count++;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
-	new_node -> next = temp -> next;
-	temp -> next = new_node;
-
+	new_node->next = temp->next;
+	temp->next = new_node;
 	return (new_node);
 }
