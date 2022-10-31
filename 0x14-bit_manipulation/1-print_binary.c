8449561fc3b0d83fbd3d3b21d69d62846b77ea92
sundayirvine-code <amugumbiirvine@gmail.com>
2022-10-31 11:41:59 -0700

    initial commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,25 +2,14 @@
 
 /**
  * print_binary - prints the binary representation of a number
- * @n: parameter
+ * @n: the number to be converted
+ *
+ * Return: Void
  */
+
 void print_binary(unsigned long int n)
 {
-	unsigned int i, count = 0;
-	unsigned long int current;
-
-	for (i = 63; i >= 0; i--)
-	{
-		current = n >> i;
-
-		if (current & 1)
-		{
-			_putchar('1');
-			count++;
-		}
-		else if (count)
-			_putchar('0');
-	}
-	if (!count)
-		_putchar('0');
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
