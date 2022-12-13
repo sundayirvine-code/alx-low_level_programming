eb3c67a054b62ff4b21a9d7af13c5d0b3e617c7a
sundayirvine-code <amugumbiirvine@gmail.com>
2022-12-13 04:57:13 -0800

    dynamic library creation

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%lu\n", strlen("My Dyn Lib"));
+    return (0);
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,2 @@
+0. Create the dynamic library libdynamic.so containing all the functions
+1. Create a script that creates a dynamic library called liball.so from all the .c files that are in the current directory.
