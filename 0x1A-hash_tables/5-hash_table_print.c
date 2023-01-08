1a902c59de5532a681f65b16d6f69a182fc07fb0
Irvine Sunday <amugumbiirvine@gmail.com>
2023-01-09 02:46:33 +0300

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,7 +7,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *temp;
 	unsigned long int i;
-	int flag;
+	int flag = 0;
 
 	if (ht == NULL)
 		return;
@@ -16,14 +16,15 @@ void hash_table_print(const hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		temp = ht->array[i];
-		flag = 0;
+
 		while (temp != NULL)
 		{
 			if (flag == 1)
 				printf(", ");
 			printf("'%s': '%s'", temp->key, temp->value);
-			flag = 1;
 			temp = temp->next;
+			if (flag == 0)
+				flag = 1;
 		}
 	}
 	printf("}\n");
